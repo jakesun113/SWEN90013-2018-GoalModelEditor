ffe25bfdd965e83032e868d69cab97924c5bf6d6
Evan <ecra93@gmail.com>
2018-04-30 04:07:38 +1000

    [Evan][GO9001318-64] Fixed typi

--- GoalModelEditor/Integration/mock-back-end/server.js
+++ GoalModelEditor/Integration/mock-back-end/server.js
@@ -7,6 +7,6 @@ const DEFAULT_PORT = 3000;
 const http = require("http");
 const app = require("./app");
 const port = process.env.PORT || DEFAULT_PORT;
-const server = http,createServer(app);
+const server = http.createServer(app);
 
 server.listen(port);
