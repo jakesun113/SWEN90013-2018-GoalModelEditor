0031e49a59ea7edfbeb499541d79dfb569a5969c
jakesun113 <jakesun113@gmail.com>
2018-04-29 20:48:43 +1000

    add jquery of profile

--- GoalModelEditor/FrontEnd/public/js/userProfile.js
+++ GoalModelEditor/FrontEnd/public/js/userProfile.js
@@ -44,42 +44,57 @@ var orpwd = document.getElementById("orpwd");
 var newpwd = document.getElementById("newpwd");
 var cfpwd = document.getElementById("cfpwd");
 
+var nametext = document.getElementById("usernametext");
+var newpwdtxt = document.getElementById("newpwdtxt");
+var cfpwdtxt = document.getElementById("cfpwdtxt");
+
+var errmsg = document.getElementById("errmsg");
+
 var fname = document.getElementById("firstname");
 var lname = document.getElementById("lastname");
-var company = document.getElementById("company");
 var email = document.getElementById("email");
 
 ccl.style.visibility = 'hidden';
 
+nametext.setAttribute("readOnly", 'true');
 fname.setAttribute("readOnly", 'true');
 lname.setAttribute("readOnly", 'true');
-company.setAttribute("readOnly", 'true');
 email.setAttribute("readOnly", 'true');
 
 button.onclick = function () {
-    Show_Hidden(username);
-    Show_Hidden(orpwd);
-    Show_Hidden(newpwd);
-    Show_Hidden(cfpwd);
 
-    Show_Cancel(ccl);
-
-    Change_Title(title);
-
-    if (button.value == "Edit") {
-        button.value = "Confirm";
-        button.type = "button";
+    if (newpwdtxt.value != cfpwdtxt.value) {
+        errmsg.style.display = 'block';
+        cfpwdtxt.focus();
+        return false;
     }
     else {
-        button.value = "Edit";
-        button.type = "submit";
+        errmsg.style.display = 'none';
 
+        Show_Hidden(username);
+        Show_Hidden(orpwd);
+        Show_Hidden(newpwd);
+        Show_Hidden(cfpwd);
+
+        Show_Cancel(ccl);
+
+        Change_Title(title);
+
+        if (button.value == "Edit") {
+            orpwd.value = "";
+            newpwdtxt.value = "";
+            cfpwdtxt.value = "";
+            button.value = "Confirm";
+            button.type = "button";
+        }
+        else {
+            button.value = "Edit";
+            button.type = "submit";
+        }
+        Change_Readonly(fname);
+        Change_Readonly(lname);
+        Change_Readonly(email);
     }
-    Change_Readonly(fname);
-    Change_Readonly(lname);
-    Change_Readonly(company);
-    Change_Readonly(email);
-
 }
 
 ccl.onclick = function () {
@@ -92,13 +107,55 @@ ccl.onclick = function () {
 
     Change_Title(title);
 
+    errmsg.style.display = 'none';
+    newpwdtxt.value = "";
+    cfpwdtxt.value = "";
+
     if (button.value == "Edit")
         button.value = "Confirm";
     else button.value = "Edit";
 
     Change_Readonly(fname);
     Change_Readonly(lname);
-    Change_Readonly(company);
     Change_Readonly(email);
 
 }
+
+$(document).ready(function () {
+    $.ajax('/user/profile/profile', {
+        type: "GET",
+        success: function (profile) {
+            nametext.value = profile[0].Username;
+            fname.value = profile[0].FirstName;
+            lname.value = profile[0].LastName;
+            email.value = profile[0].Email;
+        }
+    }).fail(function (jqXHR) {
+        alert(jqXHR.statusText + ". Please contact us.");
+    }); // end ajax
+
+}); // end ready
+
+$("#profile").submit(function (evt) {
+    evt.preventDefault();
+    var formData = $(this).serialize();
+    $.ajax('/user/profile', {
+        data: formData,
+        type: "POST",
+        success: function (result) {
+            console.log(result);
+            fname.value = result[0].FirstName;
+            lname.value = result[0].LastName;
+            email.value = result[0].Email;
+            $("#notice").slideDown().delay(3000).slideUp();
+        }
+    }).fail(function (jqXHR) {
+        alert(jqXHR.statusText + ". Please contact us.");
+    });// end ajax
+});
+
+$.validate({
+    form : '#profile',
+    validateOnBlur : false,
+    modules : 'html5'
+});
--- GoalModelEditor/FrontEnd/routes/user/profile.js
+++ GoalModelEditor/FrontEnd/routes/user/profile.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET user profile page */
 router.get('/', function(req, res, next) {
-    res.render("./user/userprofile");
+    res.render("./user/userprofile", );
 });
 
+router.get('/profile', function(req, res, next) {
+    var mockup = [{
+        "Username": "jakesun",
+        "FirstName": "Jake",
+        "LastName": "Sun",
+        "Email": "jakesun@gmail.com"
+    }];
+    res.json(mockup);
+});
+
+/* POST a new file to the server */
+router.post('/', function(req, res, next) {
+    var mockup = [{
+        "FirstName": "Just",
+        "LastName": "Test",
+        "Email": "test@gmail.com"
+    }];
+    res.json(mockup);
+});
 module.exports = router;
