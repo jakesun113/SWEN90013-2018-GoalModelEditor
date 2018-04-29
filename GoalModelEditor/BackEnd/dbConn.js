e75ab646dd15ed6bff169c46df1c8bc100f22d26
Charles TAO <ctao1@student.unimelb.edu.au>
2018-04-29 18:09:02 +1000

    Insert project into DB

--- GoalModelEditor/BackEnd/dbConn.js
+++ GoalModelEditor/BackEnd/dbConn.js
@@ -18,6 +18,15 @@ var SQL_USER_LOGIN = "UPDATE User SET LastLogin = NOW()" +
 
 var SQL_RET_USERID = "SELECT UserId from User WHERE Username = ? AND Password = ?";
 
+var SQL_RET_PROJECTID = "SELECT ProjectId from Project WHERE ProjectName = ? AND OwnerId = ?";
+
+var SQL_Project_Creation= "INSERT INTO " +
+    "Project (ProjectId, ProjectName, ProjectDescription, LastModified, Size,OwnerId) " +
+    "VALUES (UUID(), ?, ?, NOW(), ?,?)";
+
+
+
+
 var dbConn = {};
 
 dbConn.REG_SUCCESS = 1;
@@ -25,6 +34,33 @@ dbConn.REG_ALREADY_EXIST = 0;
 
 // dbConn.LOGIN_SUCCESS = 1;
 dbConn.LOGIN_INVALID = "";
+
+dbConn.CreatProject_SUCCESS=1;
+
+dbConn.createProject = function ( ProjectName, ProjectDescription, Size, UserId) {
+    var connection = mysql.createConnection(dbconf);
+    return new Promise(function (resolve, reject) {
+        connection.query(
+            SQL_Project_Creation,
+            [ProjectName, ProjectDescription, Size, UserId], function (err, result) {
+
+                if (err) {
+                    console.log(JSON.stringify(err));
+                } else {
+                    // success
+                    connection.query(SQL_RET_PROJECTID, [ProjectName, UserId], function (err, result) {
+                        if (err) return reject(err);
+                        // if success: return userid
+                        resolve(result[0].ProjectId);
+                        //console.log(result);
+                        //resolve(result.ProjectId);
+                    });
+                }
+                connection.end();
+            });
+
+    });
+}
 
 dbConn.insertUser = function (username, password, Email, FirstName, LastName) {
     var connection = mysql.createConnection(dbconf);
@@ -46,6 +82,7 @@ dbConn.insertUser = function (username, password, Email, FirstName, LastName) {
                     resolve(dbConn.REG_SUCCESS);
 
                 }
+
             });
     });
 }
@@ -75,7 +112,11 @@ dbConn.login = function (username, password) {
 }
 
 
-module.exports = dbConn;
+//module.exports = dbConn;
 // dbConn.login('qweqwe','123456').then((res)=>{
 //     console.log(res);
 // });
+
+dbConn.createProject('ProjectName4', 'ProjectDescription2', '88', '0aa452d7-4b67-11e8-8c21-02388973fed8').then((res)=>{
+     console.log(res);
+ });
