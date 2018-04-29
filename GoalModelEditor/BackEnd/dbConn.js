05aa6d4a574b4c4a59f8dc206018c83e362519a2
Xinda Yu <xinday@student.unimelb.edu.au>
2018-04-29 14:14:47 +1000

    use promise

--- GoalModelEditor/BackEnd/dbConn.js
+++ GoalModelEditor/BackEnd/dbConn.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql');
-var http = require('http');
+var Promise = require('bluebird');
 
 var dbconf = {
     host: 'ec2-52-65-15-37.ap-southeast-2.compute.amazonaws.com',
@@ -14,46 +14,51 @@ var SQL_USER_REGISTER = "INSERT INTO " +
     "VALUES (UUID(), ?, ?, ?, ?, ?, NOW(), NOW())";
 // var SQL_USER_LOGIN = "SELECT Username, Password FROM user where Username = ? AND Password = ?";
 
-var SQL_USER_LOGIN = "UPDATE user SET LastLogin = NOW()" +
+var SQL_USER_LOGIN = "UPDATE User SET LastLogin = NOW()" +
     "WHERE Username = ? AND Password = ?";
 
 
 var dbConn = {};
-dbConn.insertUser = function (username, password, Email, FirstName, LastName) {
+dbConn.insertUser = function (username, password, Email, FirstName, LastName, callback) {
     var connection = mysql.createConnection(dbconf);
-    connection.query(
-        SQL_USER_REGISTER,
-        [username, password, Email, FirstName, LastName], function (err, result) {
-            if (err) throw err;
-            connection.end();
-            console.log("connection ended\n");
-            return result;
-        });
+    return new Promise(function (resolve, reject) {
+        connection.query(
+            SQL_USER_REGISTER,
+            [username, password, Email, FirstName, LastName], function (err, result) {
+                connection.end();
+                if (err) return reject(err);
+                resolve(result);
+            });
+
+    });
 }
 
 dbConn.login = function (username, password) {
     var connection = mysql.createConnection(dbconf);
-    connection.query(
-        SQL_USER_LOGIN,
-        [username, password], function (err, result) {
-            if (err) throw err;
-            // if(result.length == 1){
-            //     console.log('successj');
-            // }else{
-            //     console.log('wrong cred');
-            // }
-            if(result.affectedRows == 1){
-                console.log('success');
-            }else{
-                console.log('wrong cred');
-            }
-            connection.end();
-        });
+    return new Promise(function (resolve, reject) {
+        connection.query(
+            SQL_USER_LOGIN,
+            [username, password], function (err, result) {
+                connection.end();
+                if (err) return reject(err);
+                console.log(result);
+                if (result.affectedRows == 1) {
+                    resolve('success');
+                } else {
+                    reject('nosuchuser');
+                }
+            })
+    });
 }
 
 
-// dbConn.login('qwe','123456');
-// dbConn.login('qwe','123456asd');
-// module.exports = dbConn;
+// console.log('before/after?');
+// dbConn.insertUser('qweqwe2', '123456', 'asd', 'aa', 'bb').then(function (result) {
+//     console.log(result);
+// });
 
-dbConn.insertUser('qweqwe','123456','asd','aa','bb');
+// dbConn.login('qweqwe2','123456').then(function (result) {
+//     console.log(result);
+// });
+
+module.exports = dbConn;
