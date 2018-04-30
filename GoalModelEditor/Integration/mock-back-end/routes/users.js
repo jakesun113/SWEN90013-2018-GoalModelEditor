afadbb714229ea3d9a3b493e073b0bcdd6c611d1
Evan <ecra93@gmail.com>
2018-04-30 15:39:00 +1000

    [Evan][GO9001318-64] Added response to user login request

--- GoalModelEditor/Integration/mock-back-end/routes/users.js
+++ GoalModelEditor/Integration/mock-back-end/routes/users.js
@@ -12,13 +12,41 @@ const router = express.Router();
 
 // (1) registration
 router.post("/", (req, res, next) => {
-    res.status(200).json({
-        message : "Confirming user " + req.body.username + " created."
-    });
+    
+    // if user does not already exist
+    if (true) {
+        res.status(201).json({
+            message : "Confirming user " + req.body.username + " created."
+        });
+    }
+
+    // if user does al;ready exist
+    else {
+        res.status(402).json({
+
+        });
+    }
 });
 
 // (2) login
-//router.get("/");
+router.get("/", (req, res, next) => {
+
+    // if valid username and password combination
+    if (true) {
+        res.status(200).json({
+            message : "Success",
+            userid : "USER-0001",
+            token : "TOKEN-USER-0001"
+        });
+    }
+
+    // if invalid username and password combination
+    else {
+        res.status(402).json({
+            message : "Failure"
+        });
+    }
+});
 
 // (3) retrieve profile
 //router.get("/");
