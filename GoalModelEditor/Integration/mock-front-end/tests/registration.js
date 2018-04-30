42ea014fc2cc9247ba00c857f3313315856a7009
Evan <ecra93@gmail.com>
2018-04-30 15:24:45 +1000

    [Evan][GO9001318-64] JSON schema for user registration added

--- GoalModelEditor/Integration/mock-front-end/tests/registration.js
+++ GoalModelEditor/Integration/mock-front-end/tests/registration.js
@@ -8,15 +8,25 @@ const BACKEND_SERVER_URL = "http://localhost:3000"
 const USER_ROUTE = "/users"
 
 // (1) register new user
+
+// the JSON for user registration
+var payload = {
+    type: "REGISTER",
+    username: "VPutin",
+    password: "qwerty",
+    firstname: "Vladimir",
+    lastname: "Putin",
+    email: "putin@kgb.ru"
+}
+
 request.post(
+    
+    // formulate and send the request
     { url: BACKEND_SERVER_URL+USER_ROUTE
     , json: true
-    , body: 
-        { username: "VPutin"
-        , password: "eye3>rusya"
-        }
-    }
-
+    , body: payload
+    
+    // callback function on response
     , function (err, res, body) {
         if (err) {
             console.log(err);
