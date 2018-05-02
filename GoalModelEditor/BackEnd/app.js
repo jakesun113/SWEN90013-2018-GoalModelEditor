1d7d125bc6a8ed1bb5c99e5020c742de0b8c13ae
Sicheng Liu <sichengl4@student.unimelb.edu.au>
2018-05-02 19:30:44 +1000

    [Marc] resolve the view directory

--- GoalModelEditor/BackEnd/app.js
+++ GoalModelEditor/BackEnd/app.js
@@ -11,19 +11,21 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 let registerRouter = require('./routes/register');
+let config = require('./config');
 
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', config.server.distFolder);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(config.server.srcFolder));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
