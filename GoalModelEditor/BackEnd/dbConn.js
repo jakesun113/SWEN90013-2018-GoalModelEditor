c299adc581144fe461da6f4caddbf54150833230
Xinda Yu <xinday@student.unimelb.edu.au>
2018-05-06 12:54:08 +1000

    added function to get project and goalmodel of a user

--- GoalModelEditor/BackEnd/dbConn.js
+++ GoalModelEditor/BackEnd/dbConn.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const Promise = require('bluebird');
 
 const dbconf = {
-    connectionLimit : 100,
+    connectionLimit: 100,
     host: 'ec2-52-65-15-37.ap-southeast-2.compute.amazonaws.com',
     user: 'test',
     password: 'SWEN90013goal!',
@@ -46,7 +46,7 @@ const SQL_Project_Creation = "INSERT INTO " +
  */
 const SQL_RET_GOALMODEL_OF_PROJ = "SELECT ModelName " +
     "FROM Project LEFT JOIN GoalModel\n" +
-    "On Project.ProjectId = GoalModel.ProjectId\n" +
+    "ON Project.ProjectId = GoalModel.ProjectId\n" +
     "WHERE Project.ProjectId = ?;";
 /**
  * The SQL sentence to create a goalmodel
@@ -54,6 +54,14 @@ const SQL_RET_GOALMODEL_OF_PROJ = "SELECT ModelName " +
  */
 const SQL_CREATE_GOALMODEL = "INSERT INTO GoalModel (ModelId,ModelName, ModelDescription, URL, ProjectId) " +
     "VALUES (UUID(), ?, ?, ?, ?)";
+/**
+ * get all project and its corresponding goalmodels
+ * @type {string}
+ */
+const SQL_GET_PROJ_GOALMODEL = "SELECT UP.ProjectId, ModelId, ModelName, ProjectName " +
+    "FROM GoalModel AS GM INNER JOIN User_Project AS UP INNER JOIN Project " +
+    "ON UP.ProjectId = GM.ProjectId AND UP.ProjectId = Project.ProjectId " +
+    "WHERE UserId = ?";
 
 const dbConn = {};
 /**
@@ -165,18 +173,51 @@ dbConn.getGoalModelList = function (ProjectId) {
             })
     });
 };
-
-dbConn.createGoalModel = function (modelName, modelDescription, url, ProjectId){
-    return new Promise(function(resolve, reject){
-        pool.query(SQL_CREATE_GOALMODEL,[modelName,modelDescription,url,ProjectId],function(err,result){
-            if(err) return reject(err);
+/**
+ * The function to create a goal model under a project.
+ * @param modelName
+ * @param modelDescription
+ * @param url
+ * @param ProjectId
+ */
+dbConn.createGoalModel = function (modelName, modelDescription, url, ProjectId) {
+    return new Promise(function (resolve, reject) {
+        pool.query(SQL_CREATE_GOALMODEL, [modelName, modelDescription, url, ProjectId], function (err, result) {
+            if (err) return reject(err);
             resolve(result);
         });
     });
-}
+};
+/**
+ * get the project and all the goal model of a user
+ * @param userid
+ */
+dbConn.getProjectGoalModelList = function (userid) {
+    return new Promise((resolve, reject) => {
+        pool.query(SQL_GET_PROJ_GOALMODEL, [userid], (err, result) => {
+            if (err) return reject(err);
+            const ret = {};
+            for (const x of result) {
+                const proj = x.ProjectName;
+                const projId = x.ProjectId;
+                if(!ret[proj]){
+                    ret[proj] = {ProjectId: projId, Models: []};
+                }
+                delete x['ProjectName'];
+                delete x['ProjectId'];
+                ret[proj]['Models'].push(x);
+            }
+            resolve(ret);
+        });
+    });
+};
 
 module.exports = dbConn;
 
-// dbConn.createGoalModel('test','test','/test','70edb3aa-4b83-11e8-8c21-02388973fed8').then((res)=>{
-//     console.log(res);
+
+
+// dbConn.getProjectGoalModelList('0aa452d7-4b67-11e8-8c21-02388973fed8').then(res => {
+//     console.log(JSON.stringify(res));
+//     pool.end();
 // });
+
