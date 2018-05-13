1fe1bb4c4801b48480e67b4fde46bf44b602dc18
Xinda Yu <xinday@student.unimelb.edu.au>
2018-05-13 12:08:29 +1000

    [Xinda]redesign db module

--- GoalModelEditor/BackEnd/DBModule/test.js
+++ GoalModelEditor/BackEnd/DBModule/test.js
@@ -4,8 +4,9 @@ var dbModule = require('./DBModule')('dev');
 
 console.log(dbModule);
 
-
-
-dbModule.getProjectGoalModelList('e06cd02b-4df4-11e8-8c21-02388973fed8').then(res => {
+dbModule.insertUser('username', 'password', 'email', 'FirstName', 'LastName').then(res => {
     console.log(res);
+    dbModule.getProjectGoalModelList('e06cd02b-4df4-11e8-8c21-02388973fed8').then(res1 => {
+        console.log(res1);
+    });
 });
