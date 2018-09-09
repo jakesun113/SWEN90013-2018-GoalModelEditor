d7f0f618c1b581ff4c89ccfbe916c9378413885b
Sicheng Liu <sichengl4@student.unimelb.edu.au>
2018-09-09 22:17:03 +1000

    [Marc][GO9001318-262] Customized images in the svg image can show now

--- GoalModelEditor/FrontEnd/public/js/projectEdit/graphXML.js
+++ GoalModelEditor/FrontEnd/public/js/projectEdit/graphXML.js
@@ -91,6 +91,7 @@ function renderFromXML(xml) {
 function exportImage() {
     let svg = document.getElementsByTagName("svg")[0];
     svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svg.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
     svg.setAttribute("version", "1.1");
     svg.setAttribute("xmlns:xlink","http://www.w3.org/1999/xlink");
     // console.log(svg.outerHTML);
